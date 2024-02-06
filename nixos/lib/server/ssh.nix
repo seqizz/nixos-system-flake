ee22fee39f186ace4568e2c13b8e60756be0ec8d
Gürkan <gitoperations@gurkan.in>
2024-02-07 00:59:37 +0100

    Add root pass for TTY login

--- nixos/lib/server/ssh.nix
+++ nixos/lib/server/ssh.nix
@@ -3,11 +3,15 @@ let
   secrets = import ../secrets.nix;
 in
 {
-  users.users.root.openssh.authorizedKeys.keys = [
-    secrets.innoSSHPub
-    secrets.plutoSSHPub
-    secrets.siktirinSSHPub
-  ];
+  users.users.root = {
+    openssh.authorizedKeys.keys = [
+      secrets.innoSSHPub
+      secrets.plutoSSHPub
+      secrets.siktirinSSHPub
+    ];
+    # For emergencies
+    hashedPassword = secrets.rocksteadyRootPass;
+  };
 
   services.openssh = {
     enable = true;
