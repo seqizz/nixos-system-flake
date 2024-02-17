976785211f9d70931c7bdfe9f4c32089ba1092fd
Gürkan <gitoperations@gurkan.in>
2024-02-17 19:06:05 +0100

    Wipe comments

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,5 +1,3 @@
-# This is your home-manager configuration file
-# Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
 {
   inputs,
   outputs,
@@ -8,42 +6,21 @@
   pkgs,
   ...
 }: {
-  # You can import other home-manager modules here
   imports = [
-    # If you want to use modules your own flake exports (from modules/home-manager):
-    # outputs.homeManagerModules.example
-
-    # Or modules exported from other flakes (such as nix-colors):
-    # inputs.nix-colors.homeManagerModules.default
-
     ./lib/common.nix
   ];
 
   nixpkgs = {
-    #   # You can add overlays here
     overlays = [
-      #     # Add overlays your own flake exports (from overlays and pkgs dir):
+      # overlays of own flake exports (from overlays and pkgs dir):
       outputs.overlays.additions
       outputs.overlays.modifications
       outputs.overlays.unstable-packages
       outputs.overlays.oldversion-packages
-      #
-      #     # You can also add overlays exported from other flakes:
-      #     # neovim-nightly-overlay.overlays.default
-      #
-      #     # Or define it inline, for example:
-      #     # (final: prev: {
-      #     #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     #     patches = [ ./change-hello-to-hi.patch ];
-      #     #   });
-      #     # })
     ];
-    #   # Configure your nixpkgs instance
     config = {
-      #     # Disable if you don't want unfree packages
       allowUnfree = true;
-      #     # Workaround for https://github.com/nix-community/home-manager/issues/2942
-      allowUnfreePredicate = _: true;
+      # allowUnfreePredicate = _: true; # XXX: remove if no issues arise
     };
   };
 
