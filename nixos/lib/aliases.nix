aa9cfba98473030b6318039c6324c0741c6ff5a8
Gürkan <gitoperations@gurkan.in>
2024-07-06 12:55:18 +0200

    Use nix-profile command to remove generations

--- nixos/lib/aliases.nix
+++ nixos/lib/aliases.nix
@@ -14,7 +14,7 @@ in
     sysup-noupdate = "sudo nixos-rebuild switch --flake path://${myConfigPath}#${config.networking.hostName} --verbose --upgrade --option eval-cache false";
     sysup = "update-flake-inputs && sysup-noupdate && if [[ $(whoami) == 'gurkan' ]]; then echo; echo \"Switching home-manager after waiting 15 sec...\"; sleep 15; homeup-noupdate; fi";
     homeup = "update-flake-inputs && homeup-noupdate";
-    sysclean = "if [[ $(whoami) == 'gurkan' ]]; then echo; echo \"Clearing home-manager...\"; home-manager expire-generations \"-10 days\"; fi; sudo nix-env -p /nix/var/nix/profiles/system --delete-generations +10 && sudo nix-collect-garbage -d; sudo nix-store --optimize";
+    sysclean = "if [[ $(whoami) == 'gurkan' ]]; then echo; echo \"Clearing home-manager...\"; home-manager expire-generations \"-10 days\"; fi; sudo nix profile wipe-history --profile /nix/var/nix/profiles/system --older-than 14d && sudo nix-collect-garbage -d; sudo nix-store --optimize";
     syslist = "echo 'System:' ; sudo nix-env -p /nix/var/nix/profiles/system --list-generations; if [[ $(whoami) == 'gurkan' ]]; then echo; echo 'Home-manager:'; home-manager generations; fi";
     pkglist = "for pack in `nixos-option environment.systemPackages | head -2 | tail -1 | tr -d \"[],\"`; do echo $pack ; done";
   };
