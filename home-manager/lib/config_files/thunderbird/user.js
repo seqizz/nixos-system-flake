886d4ff3076bcbe969d092717f36c81b0f436b26
Gürkan <gitoperations@gurkan.in>
2024-08-09 13:52:10 +0200

    thunderbird: No threads

--- home-manager/lib/config_files/thunderbird/user.js
+++ home-manager/lib/config_files/thunderbird/user.js
@@ -1,6 +1,7 @@
 user_pref("mailnews.default_news_sort_order", 2);
 user_pref("mailnews.default_news_sort_type", 18);
 user_pref("mailnews.default_sort_order", 2);
+user_pref("mailnews.default_view_flags", 0);
 user_pref("beacon.enabled", false);
 user_pref("mail.close_message_window.on_delete", true);
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
