86e609c6f9569ac510c3da466410feab6fdc8a78
Gürkan <gitoperations@gurkan.in>
2024-12-24 18:46:15 +0100

    loose: formatting

--- pkgs/loose.nix
+++ pkgs/loose.nix
@@ -1,51 +1,53 @@
-{ lib
-, pkgs
-, installShellFiles
-, python3Packages
-, inputs
-}:
-let
+{
+  lib,
+  pkgs,
+  installShellFiles,
+  python3Packages,
+  inputs,
+}: let
   pyedid = python3Packages.callPackage ./pyedid.nix {};
 in
-python3Packages.buildPythonApplication rec {
+  python3Packages.buildPythonApplication rec {
+    pname = "loose";
+    version = "master";
+    pyproject = true;
 
-  pname = "loose";
-  version = "master";
-  pyproject = true;
+    src = inputs.loose-src;
 
-  src = inputs.loose-src;
+    nativeBuildInputs = with python3Packages; [
+      poetry-core
+      installShellFiles
+    ];
 
-  nativeBuildInputs = with python3Packages; [
-    poetry-core
-    installShellFiles
-  ];
+    buildInputs = with python3Packages; [
+      shtab # For command line completion
+    ];
 
-  buildInputs = with python3Packages; [
-    shtab # For command line completion
-  ];
+    propagatedBuildInputs = with python3Packages; [
+      jc
+      pkgs.xorg.xrandr
+      pyedid
+      pyyaml
+      typing-extensions
+      xdg-base-dirs
+      yamale
+    ];
 
-  propagatedBuildInputs = with python3Packages; [
-    jc
-    pkgs.xorg.xrandr
-    pyedid
-    pyyaml
-    typing-extensions
-    xdg-base-dirs
-    yamale
-  ];
+    # Sadly shtab doesn't have fish completion yet
+    postInstall = ''
+      export HOME=$TMPDIR
+      $out/bin/loose -s bash > $HOME/loose.bash
+      $out/bin/loose -s zsh > $HOME/loose.zsh
+        installShellCompletion --cmd loose \
+          --bash $HOME/loose.bash \
+          --zsh $HOME/loose.zsh
+    '';
 
-  # Sadly shtab doesn't have fish completion yet
-  postInstall = ''
-    installShellCompletion --cmd loose \
-      --bash <($out/bin/loose -s bash) \
-      --zsh <($out/bin/loose -s zsh)
-  '';
-
-  meta = with lib; {
-    description = "Another xrandr wrapper for multi-monitor setups";
-    homepage = https://git.gurkan.in/gurkan/loose;
-    license = licenses.gpl3;
-    maintainers = [ "seqizz" ] ;
-    platforms = platforms.linux;
-  };
-}
+    meta = with lib; {
+      description = "Another xrandr wrapper for multi-monitor setups";
+      homepage = https://git.gurkan.in/gurkan/loose;
+      license = licenses.gpl3;
+      maintainers = ["seqizz"];
+      platforms = platforms.linux;
+    };
+  }
