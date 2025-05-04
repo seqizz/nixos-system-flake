398f11cf738e5bfcb1ba1378c868d513e8114167
Gürkan <gitoperations@gurkan.in>
2025-05-04 19:44:14 +0200

    meh

--- nixos/lib/server/anubis.nix
+++ nixos/lib/server/anubis.nix
@@ -1,17 +1,9 @@
-# { lib, config, pkgs, inputs,... }:
+{ lib, config, pkgs, inputs,... }:
 {
-  lib,
-  config,
-  ...
-}: {
-  # imports = [
-  # "${inputs.nixpkgs-unstable}/nixos/modules/services/networking/anubis.nix"
-  # ];
   services.anubis = {
-    # package = pkgs.unstable.anubis;
     instances."forgejo".settings = {
       TARGET = "http://${config.services.forgejo.settings.server.HTTP_ADDR}:${toString config.services.forgejo.settings.server.HTTP_PORT}";
     };
   };
-  users.users.nginx.extraGroups = ["anubis"]; # To access to the socket
+  users.users.nginx.extraGroups = [ "anubis" ]; # To access to the socket
 }
