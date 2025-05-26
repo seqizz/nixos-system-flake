11236b4ae7b2b7c2804e2d035d749345f4e054e8
Gürkan <gitoperations@gurkan.in>
2025-05-26 16:48:00 +0200

    yarr: Switch to upstream module

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -2,5 +2,5 @@
 # These should be stuff you would like to share with others, not your personal configurations.
 {
   # List your module files here
-  yarr = import ./yarr.nix;
+  # yarr = import ./yarr.nix;
 }
--- nixos/lib/server/yarr.nix
+++ nixos/lib/server/yarr.nix
@@ -7,7 +7,7 @@
   services.yarr= {
     enable = true;
     port = 3636;
-    directory = "/shared/yarr";
-    authFilePath = "/shared/.yarr-auth";
+    environmentFile = /shared/yarr/envfile;
+    authFilePath = /shared/.yarr-auth;
   };
 }
