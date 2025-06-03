ec9812fe15dadc0f1b86fc02e828f8ceabdfa759
Gürkan <gitoperations@gurkan.in>
2025-06-03 13:16:39 +0200

    IPU crap works on upstream now

--- nixos/machines/splinter.nix
+++ nixos/machines/splinter.nix
@@ -78,20 +78,21 @@ in {
       };
     };
     extraModulePackages = [fucknvidia];
-    kernelPackages = pkgs.linuxPackages_latest.extend (self: super: {
-      ipu6-drivers = super.ipu6-drivers.overrideAttrs (
-        final: previous: rec {
-          src = builtins.fetchGit {
-            url = "https://github.com/intel/ipu6-drivers.git";
-            ref = "master";
-            rev = "b4ba63df5922150ec14ef7f202b3589896e0301a";
-          };
-          patches = [
-            "${src}/patches/0001-v6.10-IPU6-headers-used-by-PSYS.patch"
-          ];
-        }
-      );
-    });
+    kernelPackages = pkgs.linuxPackages_latest;
+    # kernelPackages = pkgs.linuxPackages_latest.extend (self: super: {
+    #   ipu6-drivers = super.ipu6-drivers.overrideAttrs (
+    #     final: previous: rec {
+    #       src = builtins.fetchGit {
+    #         url = "https://github.com/intel/ipu6-drivers.git";
+    #         ref = "master";
+    #         rev = "b4ba63df5922150ec14ef7f202b3589896e0301a";
+    #       };
+    #       patches = [
+    #         "${src}/patches/0001-v6.10-IPU6-headers-used-by-PSYS.patch"
+    #       ];
+    #     }
+    #   );
+    # });
     # @Reference: In case I need to override the kernel
     # Apparently having a USB webcam was too insecure so we had to invent new shit
     # called IPU. I cannot use newer kernels since noone knows what they are doing
