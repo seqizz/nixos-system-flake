615eb70e7f4332a35365870406389b1035935d8c
Gürkan <gitoperations@gurkan.in>
2025-06-24 11:46:50 +0200

    Replace substituteAll with substitute

--- nixos/lib/helper-modules/writeSubbedBin.nix
+++ nixos/lib/helper-modules/writeSubbedBin.nix
@@ -1,10 +1,15 @@
-{ pkgs }:
-{
+{pkgs}: {
   # Wrap scripts into programs
-  writeSubbedBin = args:
-    pkgs.substituteAll (args // {
-      dir = "bin";
-      isExecutable = true;
-      shell = pkgs.bash;
-    });
+  writeSubbedBin = args: let
+    name = args.name;
+    src = args.src;
+    substitutions = builtins.removeAttrs args ["name" "src"];
+  in
+    pkgs.runCommand name {} ''
+      mkdir -p $out/bin
+      substitute ${src} $out/bin/${name} \
+        ${builtins.concatStringsSep " "
+        (pkgs.lib.mapAttrsToList (k: v: "--replace '@${k}@' '${v}'") substitutions)}
+      chmod +x $out/bin/${name}
+    '';
 }
