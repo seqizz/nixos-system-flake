bf85549779353e7512ce77ed440b27879308ae66
Gürkan <gitoperations@gurkan.in>
2026-08-31 14:46:24 +0200

    fonts: Try better settings

--- config/nixos/laptop/fonts.nix
+++ config/nixos/laptop/fonts.nix
@@ -1,38 +1,71 @@
 { pkgs, config, ... }:
 {
   fonts = {
-    fontconfig= {
+    fontconfig = {
       enable = true;
+
       defaultFonts = {
-        emoji = [ "Noto Color Emoji" ];
+        sansSerif = [
+          "Noto Sans"
+          "Liberation Sans"
+          "DejaVu Sans"
+        ];
+
+        serif = [
+          "Noto Serif"
+          "Liberation Serif"
+          "DejaVu Serif"
+        ];
+
+        monospace = [
+          "JetBrainsMono Nerd Font"
+          "FiraCode Nerd Font"
+          "DejaVu Sans Mono"
+        ];
+
+        emoji = [
+          "Noto Color Emoji"
+        ];
       };
+
       antialias = true;
       cache32Bit = true;
-      hinting.enable = true;
-      hinting.autohint = true;
+
+      hinting = {
+        enable = true;
+        autohint = false; # Prefer native font hinting; autohint can make glyphs look uneven
+        style = "slight";
+      };
+
+      subpixel = {
+        rgba = "rgb"; # Change to "bgr" only if your display panel is BGR
+      };
     };
+
     fontDir.enable = true;
+
     packages = with pkgs; [
-      comic-relief
       corefonts
+      liberation_ttf
+
+      noto-fonts
+      noto-fonts-color-emoji
+
       font-awesome_4
       font-awesome
       ionicons
-      liberation_ttf
       lineicons
-      nerd-fonts.agave
+      powerline-fonts
+
       nerd-fonts.fira-code
-      nerd-fonts.inconsolata
       nerd-fonts.jetbrains-mono
-      nerd-fonts.liberation
+      nerd-fonts.inconsolata
       nerd-fonts.dejavu-sans-mono
-      nerd-fonts.overpass
-      nerd-fonts.sauce-code-pro
+      nerd-fonts.liberation
       nerd-fonts.ubuntu
       nerd-fonts.ubuntu-mono
-      noto-fonts
-      powerline-fonts
-      twemoji-color-font
+
+      comic-relief
       victor-mono
     ];
   };
