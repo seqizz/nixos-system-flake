3daa13c5a2db0c8616d8b453dfcb6aad9070dbf6
Gürkan <gitoperations@gurkan.in>
2026-09-06 17:03:41 +0200

    disko: Remove install-only stuff

--- machines/splinter-disko.nix
+++ machines/splinter-disko.nix
@@ -26,9 +26,10 @@
                 name = "crypted";
                 extraOpenArgs = [];
                 settings = {
+                  # @Reference:
                   # if you want to use the key for interactive login be sure there is no trailing newline
                   # for example use `echo -n "password" > /tmp/secret.key`
-                  keyFile = "/tmp/secret.key";
+                  # keyFile = "/tmp/secret.key";
                   allowDiscards = true;
                 };
                 content = {
